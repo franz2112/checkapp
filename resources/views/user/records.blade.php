6c768998b5792ea3acb3be3b8659e47fd9df0c32
franz2112 <78640172+franz2112@users.noreply.github.com>
2022-12-10 18:13:55 +0800

    add rfecords

--- resources/views/user/records.blade.php
+++ resources/views/user/records.blade.php
@@ -35,7 +35,7 @@
                 <div class="card-body px-0 pt-0 pb-2">
                   <div class="table-responsive p-0">
                     <table class="table align-items-center mb-0">
-                      <thead>
+                      <thead style="font-size: 14px" >
                         <tr>
                           <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Doctor</th>
                           <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Clinic</th>
@@ -75,10 +75,10 @@
                               </p>
                             </td>
                             <td class="align-middle text-center ">
-                              <span class="text-xs text-secondary ">{{ $appointDetail->time }}</span>
+                              <span class="text-xs text-secondary "  style="font-size: 14px">{{ $appointDetail->time }}</span>
                             </td>
                             <td class="align-middle text-center">
-                              <span class="text-secondary text-xs font-weight-bold">{{ $appointDetail->date }}</span>
+                              <span class="text-secondary text-xs font-weight-bold"  style="font-size:14px">{{ date('F d, Y', strtotime($appointDetail->date)) }}</span>
                               {{-- <span class="text-secondary text-xs font-weight-bold">{{ date("F", mktime(0, 0, 0, $appointDetail->date, 1)) }}</span> --}}
                             </td>
                             <td class="align-middle text-center">
@@ -117,7 +117,7 @@
         aria-labelledby="addDoctorTitle"
         aria-hidden="true"
       >
-        <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
           <div class="modal-content" >
             <div class="modal-header" style="background-color: #35bf53; font-size: 16px">
               <div class="modal-title text-white" id="exampleModalLabel">
@@ -131,8 +131,91 @@
               ></button>
             </div>
             <div class="modal-body">
-          
-            </div>
+      
+              @foreach ($appointmentDetails as $appointments)
+              <div class="container">
+                <div class="row">
+                  <div class="col-2 p-2">
+                    <img src="/../assets/img/iconmonstr-calendar-6-240.png" class="avatar avatar-sm rounded me-2 shadow" alt="user1" style="width: 100%; object-fit:contain; opacity:0.8">
+                  </div>
+                  <div class="col-lg-6 p-3">
+                    {{ $appointments->consultation }} Consultation
+                    <hr>
+                    <div class="row">
+                      <div class="col-12">
+                        <span class="fw-semibold" style="font-size: 18px">                        
+                          {{ date('D, F d, Y', strtotime($appointDetail->date)) }}, {{ $appointments->time }} 
+                          <hr>
+                          <div class="row">
+                            <div class="col-12">
+                              @foreach ($doctorDetails as $doctor)
+                                @if ($appointDetail->doctor==$doctor->id)
+                                  <div class="row">
+                                    <div class="col-2">
+                                      <img src="/../assets/admin/img/doctorimage/{{ $doctor->file}}" class="py-2 rounded me-2 w-100" alt="user1">
+                                    </div>
+                                    <div class="col-10 ps-0">
+                                      <span class="fw-semibold"> 
+                                        <span class="fw-normal " style="font-size: 13px">Doctor</span>
+                                        <span class="text-uppercase">
+                                          {{$doctor->Dfname }}
+                                          {{$doctor->Dlname }}
+                                        </span>
+                                      </span>
+                                      <div class="row" style="font-size: 13px;">
+                                        <div class="col-12">
+                                          <span class="fst-italic">
+                                            {{$doctor->Specialization}}
+                                          </span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                @endif
+                              @endforeach
+                             </div>
+                          </div>
+                        </span>
+                        <hr>
+                        <div class="row">
+                          <div class="col-12">
+                            @foreach ($clinicDetails as $clinic)
+                              @if ($appointDetail->clinic_id==$clinic->id)
+                                <div class="row">
+                                  <div class="col-2">
+                                    <div class="text-center">
+                                      <i class="fa-solid fa-location-dot p-2" style="font-size: 20px"></i>
+                                    </div>
+                                  </div>
+                                  <div class="col-10 ps-0">
+                                    <span class="fw-semibold">{{$clinic->clinicname }}</span>
+                                    <div class="row" style="font-size: 13px;">
+                                      <div class="col-12">
+                                        {{$clinic->caddress }}
+                                       </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              @endif
+                            @endforeach
+                            <hr>
+
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-4 p-2">
+                    Reason/Complaints
+                      <div class="row">
+                        <div class="col-12">
+                          <span class="text-capitalize" style="font-size: 13px">{{ $appointments->reason }}</span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+              </div>
+              @endforeach
             
             <div class="modal-footer border-top-0">
               <button
